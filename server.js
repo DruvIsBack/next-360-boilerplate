8ef51ee2d2ed9dffb9410bce4b9f511a89ae2ee5
anirban <druvisback@gmail.com>
2019-04-23 02:54:40 +0530

    Add express api routes...

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
+/*const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
-app
-    .prepare()
+let core = require('./core');
+
+app.prepare()
     .then(() => {
         const server = express();
 
@@ -22,4 +23,37 @@ app
     .catch(ex => {
         console.error(ex.stack);
         process.exit(1)
-    });
+    });*/
+
+
+const express = require("express");
+const server_router = require('./core/router');
+const next = require("next");
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const next_app = next({dev});
+const handle = next_app.getRequestHandler();
+
+
+function createServer() {
+    const server = express();
+    server.use('/api', server_router);
+    server.get("*", (req, res) => handle(req, res));
+    return server;
+}
+
+
+next_app.prepare().then(() => {
+    const app = createServer();
+    app.listen(port);
+
+    if (dev) {
+        console.log("Development Build Running...");
+    } else {
+        console.log("Production Build Running...");
+    }
+}).catch(ex => {
+    console.error(ex.stack);
+    process.exit(1)
+});
+
